8aaac471f9fbf0e58614d29368864ab923ff5f89
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2016-09-03 08:19:29 -0700

    Update SchoolForecasting-Summary.R

--- SchoolForecasting-Summary.R
+++ SchoolForecasting-Summary.R
@@ -1,12 +1,7 @@
-#reading data file.
-f<-read.csv("d:/ForecastingSchool.csv",header = T)
-head(f)
-attach(f)
-
 ####  ploting error distribution
 ## year2014-three year ahead without zoros
 
-linear<-lm(X2014.first.grade~X2011.age.5+X2011.first.grade+X2010.first.grade)
+linear<-lm(X2013.first.grade~X2010.age.5+X2010.first.grade+X2009.first.grade)
 l<-summary(linear)
 
 Year2014three<-ceiling((l$coefficients[1,1]+( l$coefficients[2,1]*X2011.age.5)+( l$coefficients[3,1]*X2011.first.grade)+( l$coefficients[4,1]*X2010.first.grade))/29)
@@ -59,21 +54,21 @@ height2<-rbind(L,P)
 mp <- barplot(height,names.arg=c("mlr","naive"),ylim = c(0,1),xlim = c(0,5), beside = TRUE, space=c(0.5))
 text(mp, height, labels = format(height2, 4),
      pos = 3, cex = .75)
-####Regression2014 one year ahead
-linear<-lm(X2014.first.grade~X2013.age.5+X2013.first.grade+X2012.first.grade)
+#### error table 
+####Regression2014 three year ahead
+linear<-lm(X2013.first.grade~X2010.age.5+X2010.first.grade+X2009.first.grade)
 l<-summary(linear)
-Year2014one<-ceiling((l$coefficients[1,1]+( l$coefficients[2,1]*X2013.age.5)+( l$coefficients[3,1]*X2013.first.grade)+( l$coefficients[4,1]*X2012.first.grade))/29)
-# we can use predict function as well"predict(linear)", you will get same answer
-Year2014one<-replace(Year2014one, Year2014one<=0, 0) 
-D1<-ceiling(X2014.first.grade/29)-Year2014one
+Year2014three<-ceiling((l$coefficients[1,1]+( l$coefficients[2,1]*X2011.age.5)+( l$coefficients[3,1]*X2011.first.grade)+( l$coefficients[4,1]*X2010.first.grade))/29)
+Year2014three<-replace(Year2014three, Year2014three==-1, 0) 
+D1<-ceiling(X2014.first.grade/29)- Year2014three
 D11<-c(D1)
 MAE1<-mean(abs(D11))
-MAE1 # result    0.2228705
+MAE1 # result  0.3348891
 V1<-as.vector(X2014.first.grade)
-V2<-ceiling(V1/29)
-D22<-V2[!V2==0]
+V<-V1[!V1==0]
+D22<-c(ceiling(V/29))
 MAPE1<-mean(abs(D11/D22))
-MAPE1 # result  0.1119277
+MAPE1 # result 0.1625348
 L1<-length(which(D11 == -1)) 
 L2<-length(which(D11 == -2)) 
 L3<-length(which(D11 == -3)) 
@@ -84,20 +79,19 @@ L7<-length(which(D11 == -4))
 L8<-length(which(D11 == 4))
 L10<-length(which(D11 == 5))
 L11<-length(which(D11 == -5))
-L9<-length(which(D11==0)) # 2035
-L<-c(L11,L7,L3,L2,L1,L4,L5,L6,L8,L10) # 0   0   1  13 269 234  16   3   0   0
+L9<-length(which(D11==0)) # 1823
+L<-c(L11,L7,L3,L2,L1,L4,L5,L6,L8,L10) # 0   0   4  11 265 392  63   9   3   1
 
-####Naive 2014 one year ahead
-
-D2<-ceiling(X2014.first.grade/29)-ceiling(X2013.first.grade/29)
+####Naive 2014 three year ahead
+D2<-ceiling(X2014.first.grade/29)-ceiling(X2011.first.grade/29)
 D11<-c(D2)
 V1<-as.vector(X2014.first.grade)
-V2<-ceiling(V1/29)
-D22<-V2[!V2==0]
+V<-V1[!V1==0]
+D22<-c(ceiling(V/29))
 MAE1<-mean(abs(D11))
-MAE1 # result 0.2481525
+MAE1 # result 0.3380008
 MAPE1<-mean(abs(D11/D22))
-MAPE1 # result  0.1216809
+MAPE1 # result 0.1664654
 P1<-length(which(D11 == -1)) 
 P2<-length(which(D11 == -2)) 
 P3<-length(which(D11 == -3)) 
@@ -108,23 +102,25 @@ P7<-length(which(D11 == -4))
 P8<-length(which(D11 == 4))
 P10<-length(which(D11 == 5))
 P11<-length(which(D11 == -5))
-P9<-length(which(D11==0)) # 1988
-P<-c(P11,P7,P3,P2,P1,P4,P5,P6,P8,P10) # 0   0   4  16 281 253  27   2   0   0
-#### Single regression tree 2014-one year ahead
+P9<-length(which(D11==0)) # 1811
+P<-c(P11,P7,P3,P2,P1,P4,P5,P6,P8,P10) #[1]   0   3   4  53 477 193  23   5   2   0
 
-fit <- rpart(X2014.first.grade~X2013.age.5+X2012.age.5+X2011.age.5+X2010.age.5+X2013.first.grade+X2012.first.grade+X2011.first.grade+X2010.first.grade+X2009.first.grade)
+
+#### Single regression tree 2014-three years ahead
+
+fit <- rpart(X2014.first.grade~X2011.age.5+X2011.first.grade+X2010.first.grade+X2009.first.grade+X2010.age.5)
 summary(fit)
 prediction<-predict(fit)
 p<-ceiling(prediction/29)
 error<-ceiling(X2014.first.grade/29)-p
 D11<-c(error)
 MAE1<-mean(abs(D11))
-MAE1 # result 0.3788409
+MAE1 # result 0.420459
 V1<-as.vector(X2014.first.grade)
 V<-V1[!V1==0]
 D22<-c(ceiling(V/29))
 MAPE1<-mean(abs(D11/D22))
-MAPE1 # result 0.1838606
+MAPE1 # result 0.2052787
 L1<-length(which(D11 == -1)) 
 L2<-length(which(D11 == -2)) 
 L3<-length(which(D11 == -3)) 
@@ -135,52 +131,22 @@ L7<-length(which(D11 == -4))
 L8<-length(which(D11 == 4))
 L10<-length(which(D11 == 5))
 L11<-length(which(D11 == -5))
-L9<-length(which(D11==0)) # 1754
-L<-c(L11,L7,L3,L2,L1,L4,L5,L6,L8,L10) #   0   0   4  84 453 222  47   5   1   0
+L9<-length(which(D11==0)) # 1716
+L<-c(L11,L7,L3,L2,L1,L4,L5,L6,L8,L10) #   1   2   8 105 429 242  52  10   3   2
 
-#### Boosting tree 2014-one years ahead
-fit <- gbm(X2014.first.grade~X2013.age.5+X2012.age.5+X2011.age.5+X2010.age.5+X2013.first.grade+X2012.first.grade+X2011.first.grade+X2010.first.grade+X2009.first.grade, distribution="gaussian")
+#### Boosting tree 2014-three years ahead
+fit <- gbm(X2014.first.grade~X2011.age.5+X2011.first.grade+X2010.first.grade+X2009.first.grade+X2010.age.5, distribution="gaussian")
 summary(fit)
-predict.gbm <- function (object, newdata, n.trees, type = "link", single.tree = FALSE, ...) {
-  if (missing(n.trees)) {
-    if (object$train.fraction < 1) {
-      n.trees <- gbm.perf(object, method = "test", plot.it = FALSE)
-    }
-    else if (!is.null(object$cv.error)) {
-      n.trees <- gbm.perf(object, method = "cv", plot.it = FALSE)
-    }
-    else {
-      n.trees <- length(object$train.error)
-    }
-    cat(paste("Using", n.trees, "trees...\n"))
-    gbm::predict.gbm(object, newdata, n.trees, type, single.tree, ...)
-  }
-}
 predictions <- predict(fit)
 p<-ceiling(predictions/29)
 error<-ceiling(X2014.first.grade/29)-p
 D11<-c(error)
 MAE1<-mean(abs(D11))
-MAE1 # result 2.14119
+MAE1 # result 2.141968
 V1<-as.vector(X2014.first.grade)
 V<-V1[!V1==0]
 D22<-c(ceiling(V/29))
 MAPE1<-mean(abs(D11/D22))
-MAPE1 # result 1.190669
-
-#### coefficients tables!
-#2014 1-year-ahead
-linear<-lm(X2014.first.grade~X2013.age.5+X2013.first.grade+X2012.first.grade)
-l<-summary(linear)
-l#2014 2-year-ahead
-linear<-lm(X2014.first.grade~X2012.age.5+X2012.first.grade+X2011.first.grade)
-l<-summary(linear)
-#2014 3-year-aheads
-linear<-lm(X2014.first.grade~X2011.age.5+X2011.first.grade+X2010.first.grade)
-l<-summary(linear)
-#2014 4-year-ahead
-linear<-lm(X2014.first.grade~X2010.age.5+X2010.first.grade+X2009.first.grade)
-l<-summary(linear)
-
+MAPE1 # result 1.189243s
 
 
